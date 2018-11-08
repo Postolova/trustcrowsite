f284ca01740ab09651590028854cf6668078a571
Dmitriy_Egorov <egorov-by-egorov@mail.ru>
2018-11-08 19:29:31 +0300

    dont work

--- src/assets/js/works.js
+++ src/assets/js/works.js
@@ -1,12 +1,20 @@
 import './modules/slider';
+const tabs = document.querySelector('.works__slider-tabs');
 
-export function openBlock (e, blockName) {
+function openBlock (btn) {
 	// const worksSlider = document.querySelectorAll('.works__slider');
 	const worksBtns = document.querySelectorAll('.works__slider-btn');
+	const currentId = btn.getAttribute('data-tab');
 
 	for (const worksBtn of worksBtns) {
-		worksBtn.classList.remove('works__slider-btn--active');
+		worksBtn.className.replace('works__slider-btn--active', '');
 	}
-	document.getElementById(blockName).style.display = 'block';
-	e.currentTarget.className += 'works__slider-btn--active';
+	document.getElementById(currentId).style.display = 'block';
+	btn.currentTarget.className += 'works__slider-btn--active';
 };
+tabs.addEventListener('click', e => {
+	const target = e.target;
+	if (target.className === 'works__slider-btn') {
+		openBlock(target);
+	}
+});
