33293e61ddba1b62db818bc9edd0926250bd76d8
Dmitriy_Egorov <egorov-by-egorov@mail.ru>
2018-11-08 21:04:51 +0300

    made slider

--- src/assets/js/works.js
+++ src/assets/js/works.js
@@ -1,10 +1,15 @@
 import './modules/slider';
+const tabcontent = document.querySelectorAll('.works__slider');
 const tabs = document.querySelector('.works__slider-tabs');
-// .works__slider-btn--active
+
+for (let i = 0; i < tabcontent.length; i++) {
+	tabcontent[i].style.display = 'none';
+}
+
+document.getElementById('slider__tab-1').style.display = 'block';
+
 function openBlock (e) {
 	const currentID = e.target.dataset.tab;
-
-	const tabcontent = document.querySelectorAll('.works__slider');
 	const tablinks = document.querySelectorAll('.works__slider-btn');
 
 	for (let i = 0; i < tabcontent.length; i++) {
@@ -12,10 +17,13 @@ function openBlock (e) {
 	}
 
 	for (let i = 0; i < tablinks.length; i++) {
-		tablinks[i].className = tablinks[i].className.replace('works__slider-btn--active', '');
+		tablinks[i].classList.remove('works__slider-btn--active');
 	}
+
 	document.getElementById(currentID).style.display = 'block';
-	tablinks.classList.add('works__slider-btn--active');
+	if (e.target.className === 'works__slider-btn') {
+		e.target.classList.add('works__slider-btn--active');
+	}
 };
 
 tabs.addEventListener('click', e => {
